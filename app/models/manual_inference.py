9492907cfc3f051c2da18cea3d9655cda02fb956
Vinicius Mattoso <vinicius.vmrs@gmail.com>
2024-12-16 18:37:07 -0300

    [refactor] relative path

--- app/models/manual_inference.py
+++ app/models/manual_inference.py
@@ -4,7 +4,7 @@ from skimage.io import imread
 from skimage.transform import resize
 
 model = load("./knn_fashion_mnist_model.pkl")
-image = imread("C:/Users/vinicius/Documents/repositorios/ComputerVisionModelAPI/app/test/data/Trouser.png", as_gray=True)
+image = imread("../test/data/Trouser.png", as_gray=True)
 image_resized = resize(image, (32, 32)).flatten()
 image_normalized = image_resized / 255.0
 prediction = model.predict([image_normalized])
--- app/test/predict_test.py
+++ app/test/predict_test.py
@@ -3,7 +3,7 @@ import requests
 url = "http://127.0.0.1:8000/predict/"
 
 # Abra o arquivo de imagem que você quer testar
-with open("C:/Users/vinicius/Documents/repositorios/ComputerVisionModelAPI/app/test/data/Trouser.png", "rb") as image_file:
+with open("./data/Trouser.png", "rb") as image_file:
     files = {"file": image_file}
     response = requests.post(url, files=files)
 
